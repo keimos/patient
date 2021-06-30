e262a141b2ccef9b647a22b00fb7d9aba95c667e
keimos <keithmosleyjr@gmail.com>
2021-06-30 00:05:17 -0400

    Added PatientRequest class

--- src/test/kotlin/com/keimos/patient/PatientApplicationTests.kt
+++ src/test/kotlin/com/keimos/patient/PatientApplicationTests.kt
@@ -1,69 +1,12 @@
 package com.keimos.patient
 
-import com.keimos.data.Patient
-import com.keimos.data.PatientRepository
-import org.bson.types.ObjectId
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.web.server.LocalServerPort
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ExtendWith(SpringExtension::class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class PatientApplicationTests @Autowired constructor(
-	private val patientRepository: PatientRepository,
-	private val restTemplate: TestRestTemplate
-) {
-	private val defaultPatientId = ObjectId.get()
-
-	@LocalServerPort
-	protected var port: Int = 0
-
-	@BeforeEach
-	fun setUp() {
-		patientRepository.deleteAll()
-	}
-
-	private fun getRootUrl(): String? = "http://localhost:$port/patients"
-
-	private fun saveOnePatient() =
-		patientRepository.save(Patient(defaultPatientId, "Name",
-			"Description"))
+@SpringBootTest
+class PatientApplicationTests {
 
 	@Test
-	fun `should return all patients`() {
-		saveOnePatient()
-
-		val response = restTemplate.getForEntity(
-			getRootUrl(),
-			List::class.java
-		)
-		//Assertions
-		Assertions.assertEquals(200, response.statusCode.value())
-		Assertions.assertNotNull(response.body)
-		Assertions.assertEquals(defaultPatientId, response.body?.size)
-	}
-
-	@Test
-	fun `should return one patient by id`() {
-		saveOnePatient()
-
-		val response = restTemplate.getForEntity(
-			getRootUrl() + "/$defaultPatientId",
-			Patient::class.java
-		)
-		//Assertions
-		Assertions.assertEquals(200, response.statusCode.value())
-		Assertions.assertNotNull(response.body)
-		Assertions.assertEquals(defaultPatientId, response.body?.id)
-
-	}
+	fun contextLoads() {}
 
 }
