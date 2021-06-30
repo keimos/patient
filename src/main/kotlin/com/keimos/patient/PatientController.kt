aa8f27faa878664fb126aedce1f81f6fa7a65140
keimos <keithmosleyjr@gmail.com>
2021-06-30 00:11:03 -0400

    Added Post method to PatientController.ktController

--- src/main/kotlin/com/keimos/patient/PatientController.kt
+++ src/main/kotlin/com/keimos/patient/PatientController.kt
@@ -2,7 +2,9 @@ package com.keimos.patient
 
 import com.keimos.data.Patient
 import com.keimos.data.PatientRepository
+import com.keimos.requests.PatientRequest
 import org.bson.types.ObjectId
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -20,5 +22,15 @@ class PatientController(private val patientsRepository: PatientRepository) {
     fun getOnePatient(@PathVariable("id") id: String): ResponseEntity<Patient> {
         val patient = patientsRepository.findOneById(ObjectId(id))
         return ResponseEntity.ok(patient)
+    }
+
+    @PostMapping
+    fun createPatient(@RequestBody request: PatientRequest):
+    ResponseEntity<Patient> {
+        val patient = patientsRepository.save(Patient(
+            name = request.name,
+            description = request.description
+        ))
+        return ResponseEntity(patient, HttpStatus.CREATED)
     }
 }
